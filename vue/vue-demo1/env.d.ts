4db586c9697d92cb397d50540d10e4f9d7250263
lin <88231954+littlePrincessAlin@users.noreply.github.com>
2024-09-25 16:16:43 +0800

    Delete vue directory

--- vue/vue-demo1/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-// vue不是模块，所以ts会引入报错，在这里声明一下
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
